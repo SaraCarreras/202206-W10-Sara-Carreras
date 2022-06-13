82dde3401a6ce19f15821328f5cfa1c7feb25f8f
Sara Carreras <saracc621@gmail.com>
2022-06-13 20:02:33 +0200

    creating button comps

---
+++ src/components/button/button.jsx
@@ -0,0 +1,9 @@
+export function Button() {
+    return (
+        <>
+            <i className="icon gentleman__icon fas fa-check"></i>
+            <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+            <button className="button button--point-me">Point me!</button>
+        </>
+    );
+}
