f2b94b1a108f6ab831c1a4c71ea4df5a2e922ed9
Sara Carreras <saracc621@gmail.com>
2022-06-22 18:26:33 +0200

    header test finished

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import { Header } from './header';
 
-test('renders The pointing gentlemen app title', () => {
-    render(<Header />);
-    const titleElement = screen.getByText(/The pointing gentlemen/i);
-    expect(titleElement).toBeInTheDocument();
+describe('Given the component Header', () => {
+    describe('When the component is rendered', () => {
+        test('renders The pointing gentlemen app title', () => {
+            render(<Header />);
+            const titleElement = screen.getByText(/The pointing gentlemen/i);
+            expect(titleElement).toBeInTheDocument();
+        });
+    });
 });
