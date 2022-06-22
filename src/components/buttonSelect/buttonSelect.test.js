61d98d95850cdab3b47eb281c8286f96dace88f1
Sara Carreras <saracc621@gmail.com>
2022-06-22 18:40:03 +0200

    buttonSelect change naming test

--- src/components/buttonSelect/buttonSelect.test.js
+++ src/components/buttonSelect/buttonSelect.test.js
@@ -1,1 +1,20 @@
 import { render, screen } from '@testing-library/react';
+import { ButtonSelect } from './buttonSelect';
+
+describe('Given the component buttonSelect', () => {
+    describe('When the component is rendered', () => {
+        test('renders Sellect all as button name', () => {
+            render(<ButtonSelect />);
+            const buttoName = screen.getByText(/Select all/i);
+            expect(buttoName).toBeInTheDocument();
+        });
+    });
+
+    describe('When the component is selected', () => {
+        test('shows Unselect all as button name', () => {
+            render(<ButtonSelect allSelected={true} handleButton={() => {}} />);
+            const buttoNameUnselec = screen.getByText(/Unselect all/i);
+            expect(buttoNameUnselec).toBeInTheDocument();
+        });
+    });
+});
